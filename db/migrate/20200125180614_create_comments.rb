c3268614acabfd49432cd6a211f8f07721ce7c3b
PostgreSQL administrator <postgres@DESKTOP-UIJI1V3.localdomain>
2020-01-27 14:18:18 -0500

    removed line; wrong way to add reference

--- db/migrate/20200125180614_create_comments.rb
+++ db/migrate/20200125180614_create_comments.rb
@@ -3,7 +3,6 @@ class CreateComments < ActiveRecord::Migration[5.2]
     create_table :comments do |t|
       t.string :name
       t.text :content
-      t.post :belongs_to
 
       t.timestamps
     end
